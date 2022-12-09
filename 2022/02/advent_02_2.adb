1da48f39b3d8df180b997025bcf2c1f1b43cf747
Iru Cai <mytbk920423@gmail.com>
2022-12-09 17:23:09 +0800

    2022/02

---
+++ 2022/02/advent_02_2.adb
@@ -0,0 +1,48 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+with Ada.Integer_Text_IO;
+use Ada.Integer_Text_IO;
+
+procedure advent_02_2 is
+  type Shape is (Rock, Paper, Scissors);
+  -- Score_Table (opponent, self) is the score for a round
+  Score_Table: constant array (Shape, Shape) of Natural :=
+    (Rock => (Rock => 1 + 3, Paper => 2 + 6, Scissors => 3 + 0),
+     Paper => (Rock => 1 + 0, Paper => 2 + 3, Scissors => 3 + 6),
+     Scissors => (Rock => 1 + 6, Paper => 2 + 0, Scissors => 3 + 3));
+
+  Lose_Table: constant array (Shape) of Shape :=
+    (Rock => Scissors, Paper => Rock, Scissors => Paper);
+  Win_Table: constant array (Shape) of Shape :=
+    (Rock => Paper, Paper => Scissors, Scissors => Rock);
+
+  function Get_Score(S: String) return Natural is
+    opponent: Shape;
+    myself: Shape;
+  begin
+    case S(S'First) is
+      when 'A' => opponent := Rock;
+      when 'B' => opponent := Paper;
+      when 'C' => opponent := Scissors;
+      when others => raise Constraint_Error;
+    end case;
+
+    case S(S'First + 2) is
+      when 'X' => myself := Lose_Table(opponent);
+      when 'Y' => myself := opponent;
+      when 'Z' => myself := Win_Table(opponent);
+      when others => raise Constraint_Error;
+    end case;
+
+    return Score_Table(opponent, myself);
+  end;
+
+  Total_Score: Natural := 0;
+begin
+  while not End_Of_File loop
+    Total_Score := Total_Score + Get_Score(Get_Line);
+  end loop;
+
+  Put(Total_Score);
+  New_Line;
+end advent_02_2;
