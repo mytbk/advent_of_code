4b0bc6d7a443a6587afb2eab06816a93ee887968
Iru Cai <mytbk920423@gmail.com>
2023-12-04 15:18:24 +0800

    update string_util

--- util/string_util.adb
+++ util/string_util.adb
@@ -20,4 +20,44 @@ package body String_Util is
 		end if;
 		return vec;
 	end Split_String;
+   
+   procedure Find_Number(S : String; Num : out Integer; Found : out Boolean; Last : out Positive) is
+      Pos, End_Pos : Positive;
+      
+      function Is_Num_Char(C : Character) return Boolean is
+      begin
+	 return C = '-' or (C >= '0' and C <= '9');
+      end Is_Num_Char;
+   begin
+      Pos := S'First;
+      while Pos <= S'Last and then (not Is_Num_Char(S(Pos))) loop
+	 Pos := Pos + 1;
+      end loop;
+      
+      if Pos > S'Last then
+	 Found := False;
+	 return;
+      end if;
+
+      End_Pos := Pos;
+      while End_Pos <= S'Last and then Is_Num_Char(S(End_Pos)) loop
+	 End_Pos := End_Pos + 1;
+      end loop;
+      
+      Found := True;
+      Num := Integer'Value(S(Pos .. End_Pos - 1));
+      Last := End_Pos - 1;
+   end Find_Number;
+   
+   function Contains(S : String; Key : String) return Boolean is
+   begin
+      for I in S'Range loop
+	 exit when I + Key'Length - 1 > S'Last;
+	 if S(I .. I + Key'Length - 1) = Key then
+	    return True;
+	 end if;
+      end loop;
+      return False;
+   end Contains;
+
 end String_Util;
--- util/string_util.ads
+++ util/string_util.ads
@@ -9,5 +9,13 @@ package String_Util is
 
 	subtype StringVec is String_Vectors.Vector;
 
-	function Split_String(src: String; delim: Character) return StringVec;
+   function Split_String(src: String; delim: Character) return StringVec;
+   -- Split a string with a specific delimeter, return an unbounded string vector
+   
+   procedure Find_Number(S : String; Num : out Integer;
+			 Found : out Boolean; Last : out Positive);
+   -- Find a number in the string S, store the last digit position in Last
+   
+   function Contains(S : String; Key : String) return Boolean;
+   -- check if string S has Key as substring
 end String_Util;
